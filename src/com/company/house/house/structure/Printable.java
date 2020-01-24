278cce428780bc048a2a892b8b769b3e71f4f272
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-01-23 23:58:53 -0800

    Delete Printable.java

--- src/com/company/house/house/structure/Printable.java
+++
@@ -1,9 +0,0 @@
-package com.company.house.house.structure;
-
-import java.util.ArrayList;
-
-public interface Printable {
-    void printQuantityOfWindows(ArrayList<Window>windows);
-
-    void printQuantityOfDoors(ArrayList<Door>doors);
-}
